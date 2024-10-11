1a76eea7655ca8fe72c3becd172d682fab5c730a
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2024-10-11 15:13:43 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,7 +10,7 @@ export const StoreContext=createContext(null);
 const StoreContextProvider =(props)=>{
     
  const [cartItems,setCartItems]=useState({});
- const url = 'http://localhost:4000';
+ const url = "https://food-app-backend-42kj.onrender.com";
  const [token,setToken]=useState('');
  const [food_list,setFoodList]=useState([]);
 
