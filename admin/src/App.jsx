89affe7fb1aa4b5f68f5590b665fab952ce99e28
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2025-08-26 22:11:07 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url='http://localhost:4000'
+  const url='https://final-food-app-zeta.vercel.app'
   return (
     <div>
       <ToastContainer />
