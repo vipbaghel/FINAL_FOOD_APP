99dc7d07410c5016f0a2e33409498e4d1a96e5e3
vipin baghel <146691079+vipbaghel@users.noreply.github.com>
2025-08-26 22:09:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB= async ()=>{
-    await mongoose.connect('mongodb+srv://vipin:9536656604@cluster0.tmskx.mongodb.net/food-del').then(()=>console.log("DB Connected"));      // change here 'mongodb+srv://vipin:9536656604@cluster0.tmskx.mongodb.net/food-del'
+    await mongoose.connect('mongodb+srv://vipbaghel07:8fXHSA2mY7IzBobi@cluster0.tvvs0tv.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));      // change here 'mongodb+srv://vipin:9536656604@cluster0.tmskx.mongodb.net/food-del'
 
 }
